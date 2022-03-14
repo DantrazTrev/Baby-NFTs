727646eef2a88c985f990c2497a7b02781de74c9
Ayush Pratap Singh <ayushpratap16@gmail.com>
2022-03-14 18:15:00 +0530

    Degenerative Tests

--- test/BabyNFT.ts
+++ test/BabyNFT.ts
@@ -68,8 +68,8 @@ describe("Baby NFTS",function(){
      const uwus= (await uwu.getChars())[0]
      console.log(uwus);
     expect(await uwu.getUmfh(uwus)).to.equal(16)
-    expect(await uwu.getGreed(uwus)).to.equal(6)
-    expect(await uwu.getCoolness(uwus)).to.equal(10)
+     expect(await uwu.getGreed(uwus)).to.equal(10)
+    expect(await uwu.getCoolness(uwus)).to.equal(6)
     
    })
 
